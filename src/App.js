557655288592e6eba727594bdc1e99af97745469
Emmanuel Morales <emorales14@cuc.edu.co>
2025-03-27 00:02:59 -0500

    Commit 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "./componentes/LoginForm/LoginForm.jsx";
-import RegisterForm from "./componentes/Register/RegisterForm.jsx";
 import { Navbar } from "./componentes/Navbar/Navbar.jsx";
-import Home from "./componentes/Home/Home.jsx";  // 🔥 Importa el Home
+import Home from "./componentes/Home/Home.jsx";  
 
 function App() {
   return (
